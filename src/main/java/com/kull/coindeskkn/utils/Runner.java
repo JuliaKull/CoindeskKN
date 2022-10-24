2d74a0ab506eb869facd67d251a59a6c2beb281a
JuliaKull <7102777@mail.ru>
2022-10-24 19:34:42 +0300

    added logs;

--- src/main/java/com/kull/coindeskkn/utils/Runner.java
+++ src/main/java/com/kull/coindeskkn/utils/Runner.java
@@ -1,6 +1,7 @@
 package com.kull.coindeskkn.utils;
 
 import com.kull.coindeskkn.service.CurrencyService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -9,28 +10,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 import static com.kull.coindeskkn.utils.BpiUtils.checkInput;
+import static java.lang.System.in;
+
 
 @Component
+@Slf4j
 public class Runner implements CommandLineRunner {
 
     CurrencyService currencyService = new CurrencyService();
 
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args){
         try {
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
             System.out.println("Please enter in which currency you would like to receive latest BTC price and last 180 days min/max:");
-            String currency = in.readLine();
+            String currency = bufferedReader.readLine();
             checkInput(currency);
             currencyService.receiveActualCurrencyData(currency);
             currencyService.receiveCurrencyHistoricalData(currency);
-            in.close();
-
-        } catch (IOException e) {
-            throw new RuntimeException("Currency code not found");
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            bufferedReader.close();
+        } catch (Exception e) {
+           log.info ("Please, input correct currency code");
         }
     }
 }
