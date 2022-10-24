c20fe9e5bd13ce3ecb0b8fa3e5d7217af7ffa23d
JuliaKull <7102777@mail.ru>
2022-10-24 19:38:08 +0300

    added logs;

--- src/main/java/com/kull/coindeskkn/utils/BpiUtils.java
+++ src/main/java/com/kull/coindeskkn/utils/BpiUtils.java
@@ -5,21 +5,20 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class BpiUtils {
 
-        public static void checkInput(String currency){
-            try {
-            if(currency.isBlank()){
+    public static void checkInput(String currency) {
+        try {
+            if (currency.isBlank()) {
                 log.info("Currency can`t be empty");
-            }
-            else if (currency==null){
+            } else if (currency == null) {
                 log.info("Currency can`t be null");
-            }
-            else if (!currency.matches("[a-zA-Z]{3}")){
+            } else if (!currency.matches("[a-zA-Z]{3}")) {
                 log.info("Incorrect currency code");
-                }}
-            catch (Exception e){
-            log.info("Currency code not input");}
+            }
+        } catch (Exception e) {
+            log.info("Currency code not input");
         }
-        }
+    }
+}
 
 
 
